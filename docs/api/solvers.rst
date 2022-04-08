0d4249c62e627932561047bfff40f4456198eb4e
Nina Narodytska <n.narodytska@gmail.com>
2022-04-07 21:09:24 -0700

    created a clone

---
+++ docs/api/solvers.rst
@@ -0,0 +1,8 @@
+SAT solvers' API (:mod:`pysat.solvers`)
+=======================================
+
+.. automodule:: pysat.solvers
+    :inherited-members:
+    :no-special-members:
+    :exclude-members: Cadical, Gluecard3, Gluecard4, Glucose3, Glucose4, Lingeling, MapleChrono, MapleCM, Maplesat, Mergesat3, Minicard, Minisat22, MinisatGH
+
---
+++ tests/test_unique_model.py
@@ -0,0 +1,27 @@
+from pysat.solvers import Solver
+from pysat.formula import CNF
+
+solvers = ['cadical',
+           'gluecard30',
+           'gluecard41',
+           'glucose30',
+           'glucose41',
+           'lingeling',
+           'maplechrono',
+           'maplecm',
+           'maplesat',
+           'minicard',
+           'mergesat3',
+           'minisat22',
+           'minisat-gh']
+
+def test_solvers():
+    cnf = CNF(from_clauses=[[1, 2, 3], [-1, 2], [-2]])
+
+    for name in solvers:
+        with Solver(name=name, bootstrap_with=cnf) as solver:
+            assert solver.solve(), 'wrong outcome by {0}'.format(name)
+            assert solver.get_model() == [-1, -2, 3], 'wrong model by {0}'.format(name)
+
+            solver.add_clause([-l for l in solver.get_model()])
+            assert not solver.solve(), 'wrong outcome by {0}'.format(name)
