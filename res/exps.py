113ada45216343d3e4a5e25e8ad9a614ff4e4a76
Nina Narodytska <n.narodytska@gmail.com>
2022-04-11 16:03:09 -0700

    first version upper level

--- res/exps.py
+++ res/exps.py
@@ -612,10 +612,10 @@ if (process_run):
                 #print(results_eva[f])
                 
                 try:
-                    if (results_rc2comp[f][0] >  -1) and  (results_maxsatcomp[f][0] > -1):
-                        assert(results_rc2comp[f][0] == results_maxsatcomp[f][0])
-                    if (results_resrg[f][0] >  -1) and  (results_maxsatcomp[f][0] > -1):
-                        assert(results_resrg[f][0] == results_maxsatcomp[f][0])
+                    if (results_rc2comp[f][0] >  -1) and  (h_res_v4[f][0] > -1):
+                        assert(results_rc2comp[f][0] == h_res_v4[f][0])
+                    if (results_resrg[f][0] >  -1) and  (h_res_v4[f][0] > -1):
+                        assert(results_resrg[f][0] == h_res_v4[f][0])
                                             
                     # if (cashwmaxsat[0]):                
                     #     if (results_cashwmaxsat[f][0] >  -1) and  (results_maxsatcomp[f][0] > -1):
