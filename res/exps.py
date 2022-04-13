6ee323e45401af88904539d5b49b8c5957d09a59
Nina Narodytska <n.narodytska@gmail.com>
2022-04-12 22:35:25 -0700

    +

--- res/exps.py
+++ res/exps.py
@@ -298,8 +298,6 @@ if (process_run):
         if (tm > 3601):
             timetag = str(tm) + "."
         for filename in glob.iglob(root_dir + '**/**', recursive=True):
-                for rep in ["mse21_unwt_results"]:
-                    print(rep)
                     print (filename)
 
                     if(os.path.isfile(filename)):
@@ -308,7 +306,7 @@ if (process_run):
                         
 
                         known.append(filename)
-                        res_filename = filename.replace("mse21_complete_unwt", rep)
+                        res_filename = filename.replace("mse21_complete_unwt", "mse21_unwt_results")
                         #res_filename_1 =  filename.replace("mse21_complete_unwt", "mse21_complete_unwt_unzip")
                         file_name = (res_filename.split("/"))[-1]
                         file_name = file_name[0:80]
