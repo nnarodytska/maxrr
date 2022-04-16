6c84e49b687baa6f18063a8c036cec917b8ccf22
Nina Narodytska <n.narodytska@gmail.com>
2022-04-15 23:35:36 -0700

    +

--- res/exps.py
+++ res/exps.py
@@ -354,6 +354,7 @@ if (process_run):
                             assert(path.isfile(s))
                         if (rc2comp[0]):
                             s = f"{res_filename}.{rc2comp[1]}.{timetag}res"
+                            s.replace("mse21_unwt_results", "mse21_unwt_results_g4_rc2_manymrs")
                             res = copy.deepcopy(dummy)
                             results_rc2comp[file_name] = res
 
