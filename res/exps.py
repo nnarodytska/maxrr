4d2eac5815437c7e8019710ce577e2b5cffc7722
Nina Narodytska <n.narodytska@gmail.com>
2022-04-27 09:09:48 -0700

    +

--- res/exps.py
+++ res/exps.py
@@ -121,17 +121,17 @@ focus =[]
 # "kbtree9_7_3_5_30_5.wcsp.wcnf.gz"]
 
 rc2 = [False, "rc2"]
-rc2comp = [True, "rc2comp"]
+rc2comp = [False, "rc2comp"]
 cashwmaxsat = [False, "cashwmaxsat"]
 
-res_v0  = [True, "maxres","-r mr1d -y", "v0"]
-res_v1 = [True, "maxres", "-r mr1d -y -u", "v1"]
-res_v2 = [True, "maxres", "-r mr2d -y",  "v2"]
-res_v3 = [True, "maxres", "-r mr2d -y -u",  "v3"] # with closure
-res_v4 = [True, "maxres", "-r mr1b -y",  "v4"] # 
-res_v5 = [True, "maxres", "-r mr1b -y -u",  "v5"] 
-res_v6 = [True, "maxres", "-r mr2a -y ",  "v6"] 
-res_v7 = [True, "maxres", "-r mr2a -y -u",  "v7"] 
+res_v0  = [True, "maxres","-r mr1d -y --ilp=3600", "v0"]
+res_v1 = [False, "maxres", "-r mr1d -y -u", "v1"]
+res_v2 = [False, "maxres", "-r mr2d -y",  "v2"]
+res_v3 = [False, "maxres", "-r mr2d -y -u",  "v3"] # with closure
+res_v4 = [False, "maxres", "-r mr1b -y",  "v4"] # 
+res_v5 = [False, "maxres", "-r mr1b -y -u",  "v5"] 
+res_v6 = [False, "maxres", "-r mr2a -y ",  "v6"] 
+res_v7 = [False, "maxres", "-r mr2a -y -u",  "v7"] 
 
 # gurobi
 
